12ed86bb78e9e82c490906fa655473299e66ec68
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-06 22:24:16 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,3 +3,4 @@ import random
 def get_random_word():
     word_list = ["apple", "banana", "cherry", "dragonfruit", "elderberry", "fig", "grapefruit", "honeydew", "kiwi",
                  "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine",
+                 "watermelon"]
